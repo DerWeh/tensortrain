57639cf4201a6d2f72cbf2780c6fe8becdfa74b8
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-12-10 17:46:21 +0100

    Move Heisenberg to own file

--- dmrg_tn.py
+++ dmrg_tn.py
@@ -1,4 +1,4 @@
-"""Two-site DMRG calculation for spin 1/2 Heisenberg."""
+"""Two-site DMRG algorithm."""
 from __future__ import annotations
 
 import logging
@@ -224,32 +224,6 @@ class MPO(Sequence):
         )
         nodes = [combine(n1, n2) for n1, n2 in zip(mpo1, mpo2)]
         return MPO(nodes, left=left, right=right)
-
-
-def xx_mpo(size: int) -> MPO:
-    """Create MPO for XX Hamiltonian."""
-    phys_dim = 2
-    # Pauli matrices
-    # s_x = np.array([[0, 1],
-    #                 [1, 0]])
-    # s_y = np.array([[0, -1j],
-    #                 [1j, 0]])
-    # s_z = np.array([[1, 0],
-    #                 [0, -1]])
-    s_p = np.sqrt(2) * np.array([[0, 0],
-                                [1, 0]])
-    s_m = s_p.T
-    idx = np.eye(2)
-    mat = np.zeros([4, 4, phys_dim, phys_dim])
-    mat[0, :-1, :, :] = idx, s_m, s_p
-    mat[1:, -1, :, :] = s_p, s_m, idx
-    left = np.array([1, 0, 0, 0]).reshape([4, 1, 1])  # left MPO boundary
-    right = np.array([0, 0, 0, 1]).reshape([4, 1, 1])  # right MPO boundary
-    nodes = [tn.Node(mat, name=f"H{site}", axis_names=MO_AXES) for site in range(size)]
-    node_l = tn.Node(left, name="LH", axis_names=["right", "phys_in", "phys_out"])
-    node_r = tn.Node(right, name="HR", axis_names=["left", "phys_in", "phys_out"])
-    chain([node_l] + nodes + [node_r])
-    return MPO(nodes, left=node_l, right=node_r)
 
 
 class DMRG:
@@ -489,13 +463,6 @@ def mpo_operator(mpo: List[tn.Node]) -> sla.LinearOperator:
     return sla.LinearOperator(shape=(size, size), matvec=matvec_)
 
 
-def exact_energy(nsite: int) -> float:
-    """Exact ground state energy computed from free fermions."""
-    ham = np.diag(np.ones(nsite - 1), k=1) + np.diag(np.ones(nsite - 1), k=-1)
-    eigs = np.linalg.eigvalsh(ham)
-    return 2 * sum(eigs[eigs < 0])
-
-
 def setup_logging(level):
     """Set logging level and handler."""
     try:  # use colored log if available
@@ -508,47 +475,3 @@ def setup_logging(level):
             "%(log_color)s%(levelname)s%(reset)s:%(message)s"))
         LOGGER.addHandler(handler)
     LOGGER.setLevel(level)
-
-
-# example run
-if __name__ == '__main__':
-    # Set parameters
-    SIZE = 50
-    MAX_BOND_DIM = 70   # runtime should behave like O(bond_dim**3)
-    TRUNC_WEIGHT = 1e-6
-    SWEEPS = 2
-    SHOW = True
-    setup_logging(level=logging.DEBUG)  # print all
-    # setup_logging(level=logging.INFO)
-
-    # Initialize state and operator
-    mps = MPS.from_random(
-        phys_dims=[2]*SIZE,
-        bond_dims=[min(2**(site), 2**(SIZE-site), MAX_BOND_DIM//4)
-                   for site in range(SIZE-1)]
-    )
-    mpo = xx_mpo(len(mps))
-    dmrg = DMRG(mps, mpo)
-
-    # Run DMRG
-    energies, errors = [], []
-    for num in range(SWEEPS):
-        print(f"Running sweep {num}", flush=True)
-        eng, err = dmrg.sweep_2site(MAX_BOND_DIM, TRUNC_WEIGHT)
-        energies += eng
-        errors += err
-        H2 = dmrg.eval_ham2()
-        print(f"GS energy: {energies[-1]}, (max truncation {max(err)})", flush=True)
-        print(f"Estimated error {abs((H2 - eng[-1]**2)/eng[-1])}", flush=True)
-    energies = np.array(energies)
-    gs_energy = exact_energy(len(mps))
-    print(f"Final GS energy: {energies[-1]}"
-          f"\n True error: {(energies[-1] - gs_energy)/abs(gs_energy)}"
-          f" (absolute {energies[-1] - gs_energy})"
-          f" (max truncation in last iteration {max(err)})")
-    if SHOW:  # plot results
-        import matplotlib.pyplot as plt
-        plt.plot(energies - gs_energy)
-        plt.yscale("log")
-        plt.ylabel(r"$E^{\mathrm{DMRG}} - E^{\mathrm{exact}}$")
-        plt.show()
---
+++ heisenberg_xx.py
@@ -0,0 +1,84 @@
+"""Apply DMRG to the spin 1/2 Heisberg XX model."""
+import logging
+
+import numpy as np
+import tensornetwork as tn
+
+from dmrg_tn import MPS, MPO, DMRG, chain, MO_AXES, setup_logging
+
+
+def xx_mpo(size: int) -> MPO:
+    """Create MPO for XX Hamiltonian."""
+    phys_dim = 2
+    # Pauli matrices
+    # s_x = np.array([[0, 1],
+    #                 [1, 0]])
+    # s_y = np.array([[0, -1j],
+    #                 [1j, 0]])
+    # s_z = np.array([[1, 0],
+    #                 [0, -1]])
+    s_p = np.sqrt(2) * np.array([[0, 0],
+                                [1, 0]])
+    s_m = s_p.T
+    idx = np.eye(2)
+    mat = np.zeros([4, 4, phys_dim, phys_dim])
+    mat[0, :-1, :, :] = idx, s_m, s_p
+    mat[1:, -1, :, :] = s_p, s_m, idx
+    left = np.array([1, 0, 0, 0]).reshape([4, 1, 1])  # left MPO boundary
+    right = np.array([0, 0, 0, 1]).reshape([4, 1, 1])  # right MPO boundary
+    nodes = [tn.Node(mat, name=f"H{site}", axis_names=MO_AXES) for site in range(size)]
+    node_l = tn.Node(left, name="LH", axis_names=["right", "phys_in", "phys_out"])
+    node_r = tn.Node(right, name="HR", axis_names=["left", "phys_in", "phys_out"])
+    chain([node_l] + nodes + [node_r])
+    return MPO(nodes, left=node_l, right=node_r)
+
+
+def exact_energy(nsite: int) -> float:
+    """Exact ground state energy computed from free fermions."""
+    ham = np.diag(np.ones(nsite - 1), k=1) + np.diag(np.ones(nsite - 1), k=-1)
+    eigs = np.linalg.eigvalsh(ham)
+    return 2 * sum(eigs[eigs < 0])
+
+
+# example run
+if __name__ == '__main__':
+    # Set parameters
+    SIZE = 50
+    MAX_BOND_DIM = 70   # runtime should behave like O(bond_dim**3)
+    TRUNC_WEIGHT = 1e-6
+    SWEEPS = 2
+    SHOW = True
+    setup_logging(level=logging.DEBUG)  # print all
+    # setup_logging(level=logging.INFO)
+
+    # Initialize state and operator
+    mps = MPS.from_random(
+        phys_dims=[2]*SIZE,
+        bond_dims=[min(2**(site), 2**(SIZE-site), MAX_BOND_DIM//4)
+                   for site in range(SIZE-1)]
+    )
+    mpo = xx_mpo(len(mps))
+    dmrg = DMRG(mps, mpo)
+
+    # Run DMRG
+    energies, errors = [], []
+    for num in range(SWEEPS):
+        print(f"Running sweep {num}", flush=True)
+        eng, err = dmrg.sweep_2site(MAX_BOND_DIM, TRUNC_WEIGHT)
+        energies += eng
+        errors += err
+        H2 = dmrg.eval_ham2()
+        print(f"GS energy: {energies[-1]}, (max truncation {max(err)})", flush=True)
+        print(f"Estimated error {abs((H2 - eng[-1]**2)/eng[-1])}", flush=True)
+    energies = np.array(energies)
+    gs_energy = exact_energy(len(mps))
+    print(f"Final GS energy: {energies[-1]}"
+          f"\n True error: {(energies[-1] - gs_energy)/abs(gs_energy)}"
+          f" (absolute {energies[-1] - gs_energy})"
+          f" (max truncation in last iteration {max(err)})")
+    if SHOW:  # plot results
+        import matplotlib.pyplot as plt
+        plt.plot(energies - gs_energy)
+        plt.yscale("log")
+        plt.ylabel(r"$E^{\mathrm{DMRG}} - E^{\mathrm{exact}}$")
+        plt.show()

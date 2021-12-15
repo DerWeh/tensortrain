5b250c5ffbb65f892262808065c2ae06ef18b20e
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-12-15 11:15:06 +0100

    ENH: add two-site TDVP algorithm

--- tensortrain/tdvp.py
+++ tensortrain/tdvp.py
@@ -1,6 +1,9 @@
 """Time-depended variational principle for tensor trains."""
 import logging
 
+from typing import List
+
+import numpy as np
 import tensornetwork as tn
 
 import tensortrain.basics as tt
@@ -29,7 +32,7 @@ class TDVP(tt.Sweeper):
             new_node = tn.Node(
                 expm_multiply(-0.5j*time*h_eff, v0.tensor.reshape(-1)).reshape(v0.shape)
             )
-            if site == len(self.state) - 1:  # stated evolved, stop here
+            if site == len(self.state) - 1:  # state evolved, stop here
                 new_node.name = str(site)
                 new_node.add_axis_names(tt.AXES_S)
                 self.state.set_node(site, new_node)
@@ -106,3 +109,123 @@ class TDVP(tt.Sweeper):
         """Full TDVP sweep evolving 1 site at a time."""
         self.sweep_1site_right(time)
         self.sweep_1site_left(time)
+
+    def sweep_2site_right(self, time: float, max_bond_dim: int, trunc_weight: float
+                          ) -> List[float]:
+        """Sweep from left to right, evolving two sites at a once."""
+        assert self.state.center == 0, "To sweep right we start from the left"
+        assert None not in self.ham_right[1:], "We need all right parts"
+        # TODO: should we calculate norm here and use it form normalization?
+        tws: List[float] = []
+        for site in range(0, len(self.state)-1):
+            # locally optimize the state of sites `site` and `site+1`
+            mpo = [self.ham_left[site].copy(), self.ham[site].copy(),
+                   self.ham[site+1].copy(), self.ham_right[site+1].copy()]
+            tt.chain(mpo)
+            h_eff = tt.herm_linear_operator(mpo)
+            node1, node2 = self.state[site:site+2]
+            v0 = tn.contract_between(
+                node1, node2,
+                output_edge_order=[node1["left"], node1["phys"], node2["phys"], node2["right"]]
+            )
+            dbl_node = tn.Node(
+                expm_multiply(-0.5j*time*h_eff, v0.tensor.reshape(-1)).reshape(v0.shape)
+            )
+            # split the tensor and compress it moving center to the right
+            left, rs, rvh, trunc_s = tn.split_node_full_svd(
+                dbl_node, dbl_node[:2], dbl_node[2:],
+                max_singular_values=max_bond_dim, max_truncation_err=trunc_weight,
+                left_name=f"{site}L",
+            )
+            left.add_axis_names(tt.AXES_S)
+            self.update_ham_left(site+1, state_node=left)
+            # keep the norm
+            kept, trunc = np.sum(rs.tensor**2), np.sum(trunc_s**2)
+            tws.append(np.sqrt(trunc))
+            if trunc > 0:
+                rs.tensor *= np.sqrt(1 + trunc/kept)
+            right = tn.contract_between(
+                rs, rvh, name=str(site+1), output_edge_order=[rs[0], *rvh[1:]],
+                axis_names=tt.AXES_S,
+            )
+
+            if site < len(self.state) - 2:  # backward evolve center of orthogonality
+                mpo = [self.ham_left[site+1].copy(),
+                       self.ham[site+1].copy(),
+                       self.ham_right[site+1].copy()]
+                tt.chain(mpo)
+                h_eff = tt.herm_linear_operator(mpo)
+                right = tn.Node(
+                    expm_multiply(+0.5j*time*h_eff, right.tensor.reshape(-1)).reshape(right.shape),
+                    name=str(site+1), axis_names=tt.AXES_S,
+                )
+            self.state.set_range(site, site+2, [left, right])
+            self.state.center += 1
+            LOGGER.debug("Right sweep: bond-dim %3s, trunc %.3e", rs.tensor.shape[0], tws[-1])
+            if tws[-1] > trunc_weight:
+                LOGGER.warning("Max. bond dim %3d between sites %d--%d. Truncation error %.3e.",
+                               max_bond_dim, site-1, site, tws[-1])
+        return tws
+
+    def sweep_2site_left(self, time: float, max_bond_dim: int, trunc_weight: float
+                         ) -> List[float]:
+        """Sweep from right to left, evolving two sites at a once."""
+        assert self.state.center == len(self.state) - 1, "To sweep right we start from the left"
+        assert None not in self.ham_left[:-1], "We need all left parts"
+        tws: List[float] = []
+        for site in range(len(self.state)-1, 0, -1):
+            # locally optimize the state of sites `site` and `site+1`
+            mpo = [self.ham_left[site-1].copy(), self.ham[site-1].copy(),
+                   self.ham[site].copy(), self.ham_right[site].copy()]
+            tt.chain(mpo)
+            h_eff = tt.herm_linear_operator(mpo)
+            node1, node2 = self.state[site-1:site+1]
+            v0 = tn.contract_between(
+                node1, node2,
+                output_edge_order=[node1["left"], node1["phys"], node2["phys"], node2["right"]]
+            )
+            print(f"Forw-evolve {site} and {site+1}")
+            dbl_node = tn.Node(
+                expm_multiply(-0.5j*time*h_eff, v0.tensor.reshape(-1)).reshape(v0.shape)
+            )
+            # split the tensor and compress it moving center to the right
+            lu, ls, right, trunc_s = tn.split_node_full_svd(
+                dbl_node, dbl_node[:2], dbl_node[2:],
+                max_singular_values=max_bond_dim, max_truncation_err=trunc_weight,
+                right_name=f"R{site}",
+            )
+            right.add_axis_names(tt.AXES_S)
+            self.update_ham_right(site-1, state_node=right)
+            # keep the norm
+            kept, trunc = np.sum(ls.tensor**2), np.sum(trunc_s**2)
+            tws.append(np.sqrt(trunc))
+            if trunc > 0:
+                ls.tensor *= np.sqrt(1 + trunc/kept)
+            left = tn.contract_between(
+                lu, ls, name=str(site-1), output_edge_order=[*lu[:-1], ls[1]],
+                axis_names=tt.AXES_S,
+            )
+            if site > 1:  # backward evolve center of orthogonality
+                print(f"Back-evolve {site-1}")
+                mpo = [self.ham_left[site-1].copy(),
+                       self.ham[site-1].copy(),
+                       self.ham_right[site-1].copy()]
+                tt.chain(mpo)
+                h_eff = tt.herm_linear_operator(mpo)
+                left = tn.Node(
+                    expm_multiply(+0.5j*time*h_eff, left.tensor.reshape(-1)).reshape(left.shape),
+                    name=str(site-1), axis_names=tt.AXES_S,
+                )
+            self.state.set_range(site-1, site+1, [left, right])
+            self.state.center -= 1
+            LOGGER.debug("Left sweep: bond-dim %3s, trunc %.3e", ls.tensor.shape[0], tws[-1])
+            if tws[-1] > trunc_weight:
+                LOGGER.warning("Max. bond dim %3d between sites %d--%d. Truncation error %.3e.",
+                               max_bond_dim, site-1, site, tws[-1])
+        return tws
+
+    def sweep_2site(self, time: float, max_bond_dim: int, trunc_weight: float):
+        """Full TDVP sweep evolving 2 sites at once."""
+        tw_right = self.sweep_2site_right(time, max_bond_dim, trunc_weight)
+        tw_left = self.sweep_2site_left(time, max_bond_dim, trunc_weight)
+        return tw_right + tw_left
--- test/tdvp_test.py
+++ test/tdvp_test.py
@@ -7,7 +7,7 @@ import tensortrain as tt
 
 
 def test_tdvp1_siam_gs_evolution():
-    """Test that the Ground state energy is correct for the SIAM.
+    """Test single-site TDVP for the ground state of the SIAM.
 
     For the ground state, the time evolution should be just the GS energy.
     """
@@ -47,3 +47,46 @@ def test_tdvp1_siam_gs_evolution():
     tevo.sweep_1site(time_step)
     overlap = tt.inner(dmrg.state, tevo.state)
     assert_allclose(overlap, np.exp(-2.0j*time_step*gs_energy), rtol=1e-14)
+
+
+def test_tdvp2_siam_gs_evolution():
+    """Test two-site TDVP for the ground state of the SIAM.
+
+    For the ground state, the time evolution should be just the GS energy.
+    """
+    max_bond_dim = 50   # runtime should be have like O(bond_dim**3)
+    trunc_weight = 1e-10
+    sweeps = 3
+
+    # Initialize state and operator
+    bath_size = 10  # for some reason it is bad for small systems
+    e_onsite = 0
+    e_bath = np.linspace(-2, 2, num=bath_size)
+    hopping = np.ones(bath_size)
+    ham = tt.siam.siam_hamiltonain(e_onsite, interaction=0, e_bath=e_bath, hopping=hopping)
+    mps = tt.State.from_random(
+        phys_dims=[2]*len(ham),
+        bond_dims=[min(2**(site), 2**(len(ham)-site), max_bond_dim//4)
+                   for site in range(len(ham)-1)]
+    )
+    dmrg = tt.DMRG(mps, ham)
+
+    # Run DMRG
+    for __ in range(sweeps):
+        eng, __ = dmrg.sweep_2site(max_bond_dim, trunc_weight)
+
+    gs_energy = eng[-1]
+    tevo = tt.TDVP(state=dmrg.state, ham=dmrg.ham)
+    time_step = 0.1
+    tevo.sweep_2site_right(time_step, max_bond_dim=max_bond_dim, trunc_weight=1e-8)
+    # one sweep is half a time evolution
+    overlap = tt.inner(dmrg.state, tevo.state)
+    assert_allclose(overlap, np.exp(-0.5j*time_step*gs_energy), rtol=1e-12)
+    tevo.sweep_1site_left(time_step)
+    # another half step
+    overlap = tt.inner(dmrg.state, tevo.state)
+    assert_allclose(overlap, np.exp(-1.0j*time_step*gs_energy), rtol=1e-12)
+    # do a full sweep
+    tevo.sweep_1site(time_step)
+    overlap = tt.inner(dmrg.state, tevo.state)
+    assert_allclose(overlap, np.exp(-2.0j*time_step*gs_energy), rtol=1e-12)

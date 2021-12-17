1163817a09b2f6fc77103199cc251aa9f34921af
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-12-17 21:17:04 +0100

    TST: test trace calculation

--- test/basics_test.py
+++ test/basics_test.py
@@ -1,4 +1,5 @@
 """Very crude tests for some basic functionality."""
+import numpy as np
 import pytest
 
 import tensortrain as tt
@@ -25,3 +26,19 @@ def test_equality():
     assert bra == ket, "Random state is real."
     ket[0].tensor += 1
     assert bra != ket
+
+
+def test_operator_trace():
+    """Test trace for simple example."""
+    rng = np.random.default_rng(0)
+    sites = 4
+    parameters = {
+        "e_onsite": np.array(0.2),
+        "e_bath": 2*rng.random(sites) - 1,
+        "hopping": rng.random(sites),
+    }
+    ham = tt.siam.siam_hamiltonain(**parameters, interaction=0)
+    ham_op = tt.herm_linear_operator([ham.left] + ham.nodes + [ham.right])
+    trace = tt.basics.trace([ham.left] + ham.nodes + [ham.right])
+    dense = ham_op @ np.eye(*ham_op.shape)
+    assert trace == pytest.approx(np.trace(dense))
